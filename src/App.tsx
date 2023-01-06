dc0d8f781109583da594bf4612c3127d2608739a
forturegrant <1209095474@qq.com>
2023-01-06 15:35:37 +0800

    feat: best Search

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./header";
+import Search from "./search";
+import SearchResult from "./searchResult";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Search />} />
+        <Route path="/search/result/:searchContent" element={<SearchResult />}></Route>
+      </Route>
+    </Routes>
   );
 }
 
