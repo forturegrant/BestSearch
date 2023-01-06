1a02209227fb7af431ccb50c77600c6ce26836e6
forturegrant <1209095474@qq.com>
2023-01-06 15:51:35 +0800

    feat: maintain history

--- src/header.tsx
+++ src/header.tsx
@@ -10,7 +10,7 @@ function Header() {
     <React.Fragment>
       <header style={{ maxWidth: "1200px" }}>
         <div style={{ display: "flex", padding: '10px 20px', alignItems: 'center' }}>
-          <div style={{ marginRight: '20px' }} onClick={() => navigate('/', {replace: true})}>
+          <div style={{ marginRight: '20px' }} onClick={() => navigate('/')}>
             <span style={{ fontWeight: 600 }}>Best</span>
             <span>Search</span>
           </div>
